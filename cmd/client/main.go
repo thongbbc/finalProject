b6b47ddc81a7a5e4a1ecb3858c030a44f20576b7
TuanVo <94voanhtuan@gmail.com>
2020-07-04 11:44:42 +0700

    update flag commandline

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,25 +3,29 @@ package main
 import (
 	"context"
 	product "finalProject/cmd/service/proto"
+	"flag"
 	"fmt"
+
 	"google.golang.org/grpc"
 )
 
 func main() {
 	server := "0.0.0.0:5000"
+	id := *flag.Int("get", 1, "an int")
+	flag.Parse()
 	conn, err := grpc.Dial(server, grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
 	client := product.NewProductServiceClient(conn)
 	testAdd(client)
-	testGet(client)
+	testGet(client, int32(id))
 }
 
 func testAdd(client product.ProductServiceClient) {
 	addReq := product.AddReq{
-		Name: "iphone6",
-		Sku: "IP6",
+		Name:  "iphone6",
+		Sku:   "IP6",
 		Price: 1000000,
 	}
 	addRes, err := client.Add(context.TODO(), &addReq)
@@ -31,9 +35,9 @@ func testAdd(client product.ProductServiceClient) {
 	fmt.Println("Response of add method is:", addRes)
 }
 
-func testGet(client product.ProductServiceClient) {
+func testGet(client product.ProductServiceClient, id int32) {
 	findReq := product.GetReq{
-		Id:1,
+		Id: id,
 	}
 	findRes, err := client.Get(context.TODO(), &findReq)
 
